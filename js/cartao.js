2b20cf6776e85c8fe219be171dab9d2bda11e7a4
Alexandre Belchior <alexbelchior@gmail.com>
2019-02-25 16:37:55 -0300

    Add files via upload

---
+++ js/cartao.js
@@ -0,0 +1,23 @@
+;(function(){
+    const cartoes = document.querySelectorAll(".cartao")
+
+    for(let j=0; j < cartoes.length; j++){
+        const cartao = cartoes[j]
+
+        cartao.addEventListener("focusin",function(){
+            cartao.classList.add("cartao--focado")
+        })
+
+        cartao.addEventListener("focusout",function(){
+            cartao.classList.remove("cartao--focado")
+        })
+
+        cartao.addEventListener("change",function mudaCor(event){
+            const elementoSelecionado = event.target
+            const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
+            if(isRadioTipo){
+                cartao.style.backgroundColor = elementoSelecionado.value
+            }
+        })
+    }
+})()
